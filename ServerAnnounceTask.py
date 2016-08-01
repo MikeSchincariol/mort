08e92bbc1c6f9ab9dd9f43413f0dc820edf2019e
Mike Schincariol <mikeschincariol@gmail.com>
2016-07-31 23:03:47 -0400

    Added a class to handle sending the session-server announce message. Started work on the mort session-server.

---
+++ ServerAnnounceTask.py
@@ -0,0 +1,55 @@
+import threading
+import socket
+import time
+
+
+class ServerAnnounceTask(threading.Thread):
+    """
+    A class that runs as a thread and handles sending server-announce
+    messages at repetitive intervals.
+    """
+
+    def __init__(self, announce_hostname, announce_ip_address, announce_port):
+        """
+        Class constructor
+
+        :param announce_hostname: The hostname of the session server to announce.
+        :param announce_ip_address: The IP address string of the session server to announce.
+        :param announce_port: The port the session-server is listening on.
+        """
+        # Give a name to this thread and make it a daemon so it
+        # doesn't prevent the caller from exiting.
+        super().__init__(name="announce_thread", daemon=True)
+
+        # Store up the data for the announce message
+        self.ip_address = announce_ip_address
+        self.hostname = announce_hostname
+        self.port = announce_port
+        self.msg = ("msg_type:session_server_announce\n"
+                    "ip_address:{0}\n"
+                    "hostname:{1}\n"
+                    "port:{2}\n".format(self.ip_address,
+                                        self.hostname,
+                                        self.port))
+
+        # Get a persistent UDP socket for sending the messages
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+
+    def run(self):
+        """
+        Handles constructing an announce message and sending it.
+        Then pauses a set amount of time before sending another message.
+        :return:
+        """
+        while True:
+            self.sock.sendto(self.msg.encode('utf8'), ("<broadcast>", 42124))
+            time.sleep(10)
+
+
+if __name__ == "__main__":
+    task = ServerAnnounceTask('test-session_server', '192.168.43.12', '23234')
+    task.start()
+    task.join()
---
+++ mort_session_server.py
@@ -0,0 +1,79 @@
+import sys
+import socket
+import errno
+import select
+
+import ServerAnnounceTask
+
+
+def startup():
+
+    my_hostname = socket.gethostname()
+    my_ip_address = socket.gethostbyname(my_hostname)
+    my_port = 42124
+
+    # Open a TCP socket to listen for service requests.
+    # NOTE: The port is not important because the actual port used
+    #       will be communicated to the launcher via a the
+    #       session server announce message.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Attempt to bind to a socket at TCP:42124. However, if that
+    # port is already taken, that is not a problem, try the next
+    # port in sequence until we find one, or, we run out of ports
+    # to try (in this case, we stop if a port hasn't been found
+    # by port 59999)
+    while True:
+        if my_port <= 59999:
+            try:
+                sock.bind((my_ip_address, my_port))
+                print("Opened socket at TCP:{0}\n".format(my_port))
+                break
+            except OSError as ex:
+                if ex.errno == errno.EADDRINUSE:
+                    my_port += 1
+                else:
+                    raise
+        else:
+            print("Could not find an open port in the range 42124 to 59999.\n")
+            print("Session server exiting.\n")
+            sys.exit(1)
+
+    # Start the session-server announce thread
+    announce_task = ServerAnnounceTask.ServerAnnounceTask(my_hostname, my_ip_address, my_port)
+    announce_task.start()
+
+    # Start listening for service requests
+    sock.setblocking(False)
+    sock.listen(5)
+
+    # Enter the service loop
+    conn, remote_addr = sock.accept()
+    print ("Accepted connection from: {}".format(remote_addr))
+    while True:
+        # Get the request datagram
+        robj, wobj, xobj = select.select([conn], [], [])
+
+        # Determine what to do
+
+
+        # Do it...
+
+
+        # Send respose back
+
+
+
+
+# How do shut down?
+#    Need to implement signal handling logic....
+
+# Logging?
+#
+
+
+
+
+if __name__ == "__main__":
+    startup()
